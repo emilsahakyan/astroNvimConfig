d15f6a2bf96782fc40695795cf2c9f83be0991f5
Emil Mamajanyan <emil@vorak-solutions.com>
2023-03-22 18:48:55 +0400

    Setup lualine & toggleterm

---
+++ plugins/lualine.lua
@@ -0,0 +1,23 @@
+return {
+    {
+        "nvim-lualine/lualine.nvim",
+        event = "VeryLazy",
+        opts = {
+            options = {
+                theme = "nord",
+                icons_enabled = true,
+                component_separators = "", -- { left = '', right = ''},
+                section_separators = { left = "", right = "" },
+                --padding = 1,
+                --always_divide_middle = true,
+            },
+            sections = {
+                lualine_c = { "%=", { "filename", file_status = true, path = 3 } },
+            },
+        },
+    },
+    {
+        "rebelot/heirline.nvim",
+        opts = function(opts) opts.statusline = nil end,
+    },
+}
