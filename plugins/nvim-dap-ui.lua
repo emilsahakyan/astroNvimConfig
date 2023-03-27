53ac40f923f805e7208269c49c11045849df2fd4
Emil Mamajanyan <emil@vorak-solutions.com>
2023-03-27 14:23:14 +0400

    DAP/UI config

---
+++ plugins/nvim-dap-ui.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "rcarriga/nvim-dap-ui",
+    config = function(plugin, opts)
+      -- run default AstroNvim nvim-dap-ui configuration function
+      require "plugins.configs.nvim-dap-ui" (plugin, opts)
+
+      -- disable dap events that are created
+      local dap = require "dap"
+      local dapui = require "dapui"
+      dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+      dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+      dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+    end,
+  },
+}
